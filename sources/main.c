04c9b92214a9932301f9dd6750d5ac51a51a4502
Jules <jubernard@4bc0c31.42berlin.de>
2023-06-23 17:33:51 +0200

    write fails

--- sources/main.c
+++ sources/main.c
@@ -12,17 +12,32 @@
 
 #include "minishell.h"
 
+int	prompt(void)
+{
+	char *input;
+	int	history;
+
+	history = open("history.txt", O_CREAT | O_TRUNC, 999);
+	if (history == -1)
+		return(0);
+	input = readline("[Minishell] >");
+	int rt = write(history, input, ft_strlen(input));
+	ft_printf("write return = %i\n", rt);
+	//add_history(input);
+	close(history);
+	free(input);
+	return (0);
+}
+
 int main(void)
 {
-    char *input;
 
 	print_opening();
-    while(1)
-    {
-        input = readline("[Minishell] >");
-        add_history(input);
-    }
-    free(input);
-    return(0);
+	while(1)
+	{
+		prompt();
+	}
+
+	return(0);
 }
 
