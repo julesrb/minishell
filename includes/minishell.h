48c39299a3a7c32ac00b584e54c26ac3fb90453c
Jules <jubernard@4bc0c31.42berlin.de>
2023-06-23 14:47:44 +0200

    merged with makefile

--- includes/minishell.h
+++ includes/minishell.h
@@ -14,6 +14,10 @@
 # define MINISHELL_H
 
 # include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 # include "libft.h"
 
 
--- sources/main.c
+++ sources/main.c
@@ -12,8 +12,21 @@
 
 #include "minishell.h"
 
-int	main(void)
+int main(void)
+{
+    char *input;
+
+    while(1)
+    {
+        input = readline("[Minishell] >");
+        add_history(input);
+    }
+    free(input);
+    return(0);
+}
+
+/* int	main(void)
 {
 	ft_printf("/ - Welcome to minishell - /\n");
 	return (EXIT_SUCCESS);
-}
+} */
