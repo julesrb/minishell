2e0769a7f7391ca88acd8de2230a51b62f3b42d1
Guillaume Bussiere <gbussier@c4c3c1.42berlin.de>
2023-07-05 18:36:47 +0200

    relative path with ./NAME are working

--- sources/find_executable.c
+++ sources/find_executable.c
@@ -3,9 +3,17 @@
 char    *ft_relative_path(char *cmd)
 {
     char *relative_path = NULL;
+    char *path_trim = NULL;
+    char *env_path;
 
     if (ft_strncmp(cmd, "./", 2) == 0)
-        relative_path = ft_strjoin(getcwd(relative_path, ft_strlen(relative_path)), ft_strnstr(cmd, "./", ft_strlen(cmd)));
+    {
+        path_trim = ft_strtrim(cmd, "./");
+        env_path = ft_strdup(getenv("PWD"));
+        relative_path = ft_strjoin(env_path, (char*)"/");
+        relative_path = ft_strjoin(relative_path, path_trim);
+        printf("relative path %s\n", relative_path);
+    }
     return(relative_path);
 }
 
@@ -27,8 +35,10 @@ char *find_executable(char **cmd)
 {
     if (is_absolute_path(cmd[0]) == EXIT_SUCCESS)
         return(cmd[0]);
-/*     else if(is_relative_path(cmd[0]) == EXIT_SUCCESS)
-        return(ft_relative_path(cmd[0])); */
+    else if(is_relative_path(cmd[0]) == EXIT_SUCCESS)
+    {
+        return(ft_relative_path(cmd[0]));
+    }
     else
         return(ft_access_path(cmd, 0));
     return (NULL);
