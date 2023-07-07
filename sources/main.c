c82c061c1d3c984a4ace2c3724c39a44859ca260
Guillaume Bussiere <gbussier@c4c4c1.42berlin.de>
2023-07-07 12:59:06 +0200

    minishell 1

--- sources/main.c
+++ sources/main.c
@@ -34,11 +34,11 @@ int	init_t_mini(t_minishell *mini, char **envp)
 
 int	main(int argc, char **argv, char **envp)
 {
-	struct sigaction	sa_main;
+	//struct sigaction	sa_main;
 	t_minishell			mini;
 
-	signal_main(&mini, &sa_main);
-	signal(SIGQUIT, SIG_IGN);
+	//signal_main(&mini, &sa_main);
+	//signal(SIGQUIT, SIG_IGN);
 	mini.exit_status = 0;
 	arg_check(argc, argv);
 	if (env_mini(&mini, envp) == EXIT_FAILURE)
@@ -49,10 +49,10 @@ int	main(int argc, char **argv, char **envp)
 		init_t_mini(&mini, envp);
 		prompt(&mini);
 		lexer(&mini);
- 		//	print_lst(mini.lexer_table); 
+ 			print_lst(mini.lexer_table); 
 		parser(&mini);
- 		//	print_t_mini(&mini);
-		//	print_cmd_table(&mini, mini.nb_cmd);
+ 			print_t_mini(&mini);
+			print_cmd_table(&mini, mini.nb_cmd);
  		if ((mini.error_pipe == 0 && mini.error_redir == 0) && mini.nb_cmd > 0)
 		{
 			mini.exit_status = executor(&mini, envp);
