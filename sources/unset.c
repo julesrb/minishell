72282b0b5eb918104b12443444d9fada88b6091d
Guillaume Bussiere <gbussier@c4b4c1.42berlin.de>
2023-07-11 19:35:04 +0200

    norminette pwd and unset functions

--- sources/unset.c
+++ sources/unset.c
@@ -1,51 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbussier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 19:30:29 by gbussier          #+#    #+#             */
+/*   Updated: 2023/07/11 19:32:08 by gbussier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int  check_unset_var(char *cmd, t_list *env_mini)
+int	check_unset_var(char *cmd, t_list *env_mini)
 {
-    while (env_mini != NULL)
-    {
-        if (ft_strncmp(cmd, (char *)(env_mini->content), strlen(cmd)) == 0)
-            return (EXIT_SUCCESS);
-        env_mini = env_mini->next;
-    }
-    return (EXIT_FAILURE);
+	while (env_mini != NULL)
+	{
+		if (ft_strncmp(cmd, (char *)(env_mini->content), strlen(cmd)) == 0)
+			return (EXIT_SUCCESS);
+		env_mini = env_mini->next;
+	}
+	return (EXIT_FAILURE);
 }
 
-int     lst_env_remove(t_minishell *mini, char *var_unset)
+int	lst_env_remove(t_minishell *mini, char *var_unset)
 {
-    t_list *prev;
-    t_list *curr;
+	t_list	*prev;
+	t_list	*curr;
 
-    prev = NULL;
-    curr = mini->env_mini;
-    while (curr != NULL)
-    {
-        if (ft_strncmp(var_unset, (char *)(curr->content), ft_strlen(var_unset)) == 0)
-        {
-            if (prev == NULL)
-                mini->env_mini = curr->next;
-            else
-                prev->next = curr->next;
-            free(curr->content);
-            free(curr);
-            return (EXIT_SUCCESS);
-        }
-        prev = curr;
-        curr = curr->next;
-    }
-    return EXIT_FAILURE;
+	prev = NULL;
+	curr = mini->env_mini;
+	while (curr != NULL)
+	{
+		if (ft_strncmp(var_unset, (char *)(curr->content), ft_strlen(var_unset)) == 0)
+		{
+			if (prev == NULL)
+				mini->env_mini = curr->next;
+			else
+				prev->next = curr->next;
+			free(curr->content);
+			free(curr);
+			return (EXIT_SUCCESS);
+		}
+		prev = curr;
+		curr = curr->next;
+	}
+	return (EXIT_FAILURE);
 }
 
-int     unset_builtin(char **cmd, t_minishell *mini)
+int	unset_builtin(char **cmd, t_minishell *mini)
 {
-    int i;
+	int	i;
 
-    i = 1;
-    while (cmd[i] != NULL)
-    {
-        if (check_unset_var(cmd[i], mini->env_mini) == EXIT_SUCCESS)
-            lst_env_remove(mini, cmd[i]);
-        i++;
-    }
-    return (EXIT_SUCCESS);
+	i = 1;
+	while (cmd[i] != NULL)
+	{
+		if (check_unset_var(cmd[i], mini->env_mini) == EXIT_SUCCESS)
+			lst_env_remove(mini, cmd[i]);
+		i++;
+	}
+	return (EXIT_SUCCESS);
 }
